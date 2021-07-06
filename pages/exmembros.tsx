1aae66bb5e5ec808be73e0a8bd172f1a8af35506
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-07-06 20:09:17 -0300

    Algumas mudanças no estilo da pag. de Ex-membros + Teste

--- pages/exmembros.tsx
+++ pages/exmembros.tsx
@@ -9,17 +9,28 @@ export default function ExMembros({dados}) {
           <main className={styles.main}>
           <h1 className={styles.title}> Ex-membros</h1>
           </main>
+
+          <div className={styles.test}>
+          <div className={styles.descript}> O COMPET tem como objetivo o crescimento dos seus integrantes, suas contribuições aos colegas de curso, 
+          ao curso, à instituição e à sociedade, de forma a atuarem em ensino, pesquisa e extensão. Desde sua criação o COMPET foi contemplado
+          com a participação de diversos membros que contribuíram para o crescimento do grupo: </div>
+          </div>
+          
           {dados.map(data => ( data.email == "" ? data.email = '-' : 
             data.linkedin == "" ? data.linkedin = '-' : 
             data.data_inicio.lenght  ? data.data_inicio = '-' :  
             data.membro_ativo == false ?
+
+
             <div className={styles.membros}> 
+            <div></div>
             <p><strong>Nome: </strong> {data.nome} </p>
             <p><strong>Email: </strong> {data.email}</p>
             <p><strong>Linked-In: </strong> {data.linkedin}</p>
-            <p><strong>Entrada: </strong> {data.data_inicio}</p>  
+            <p><strong>Entrada: </strong> {(data.data_inicio)}</p>  
             <p><strong>Saída: </strong> {data.data_fim}</p>          
-            </div> : <div></div>        
+            </div> : <div></div>
+                        
           ))}
       </div>
     )
@@ -34,5 +45,3 @@ ExMembros.getInitialProps = async () => {
     console.log(response);  
     return { dados: response.data }
   };
-
-
