2c43e133e728dc9cb2643d5b7d7ebc6ea7d6f5ad
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-07-07 22:41:35 -0300

    Aproximando Front-end ao que foi proposto

--- pages/exmembros.tsx
+++ pages/exmembros.tsx
@@ -8,25 +8,23 @@ export default function ExMembros({ dados }) {
       <title>COMPET | Ex-membros</title>
       <h1 className={styles.title}> Ex-membros</h1>
 
-      <div className={styles.descript}> O COMPET tem como objetivo o crescimento dos seus integrantes, suas contribuições aos colegas de curso,
-        ao curso, à instituição e à sociedade, de forma a atuarem em ensino, pesquisa e extensão. Desde sua criação o COMPET foi contemplado
-        com a participação de diversos membros que contribuíram para o crescimento do grupo: </div>
+      <div className={styles.bodyGroup}>
+        <div className={styles.espHorizont}></div>
+        <div className={styles.membersArea}>
+          {dados.map(data => (data.email == "" ? data.email = '-' : data.email = data.email,
+            data.linkedin == "" ? data.linkedin = '-' : data.linkedin = data.linkedin,
+            data.membro_ativo == false ?
 
+              <div className={styles.membersCard}>
+                <div></div>
+                <p className={styles.infoName}> <strong>{data.nome}</strong></p>
+                <p className={styles.infoCompet}> COMPET <strong>{ }</strong></p>
+              </div> : <></>
 
-      {dados.map(data => (cont = cont + 1, data.email == "" ? data.email = '-' : data.email = data.email,
-        data.linkedin == "" ? data.linkedin = '-' : data.linkedin = data.linkedin,
-        data.membro_ativo == false ?
-
-          <div className={styles.membros}>
-            <div></div>
-            <p><strong>Nome: </strong> {data.nome} </p>
-            <p><strong>Email: </strong> {data.email}</p>
-            <p><strong>Linked-In: </strong> {data.linkedin}</p>
-            <p><strong>Entrada: </strong> {(data.data_inicio)}</p>
-            <p><strong>Saída: </strong> {data.data_fim}</p>
-          </div> : <></>
-
-      ))}
+          ))}
+        </div>
+        <div className={styles.espHorizont}></div>
+      </div>
     </div>
   )
 }
@@ -37,8 +35,6 @@ ExMembros.getInitialProps = async () => {
   const response = await axios.get(
     'http://localhost:3000/api/membros'
   );
-  console.log(response);
+  //console.log(response);
   return { dados: response.data }
 };
-
-let cont = 0;
