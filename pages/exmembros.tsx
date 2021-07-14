e450926590561f8150b8e8f90a101592e9d93312
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-07-14 16:14:18 -0300

    Separação do 'Menu' em um .html/.css especifico para import em outras paginas

--- pages/exmembros.tsx
+++ pages/exmembros.tsx
@@ -1,38 +1,22 @@
 import axios from 'axios';
 import styles from '../styles/ExMembros.module.css'
 import Link from 'next/link'
+import Menu from './menu'
 
 export default function ExMembros({ dados }) {
   return (
 
     <div className={styles.groupDiv}>
       <title>COMPET | Ex-membros</title>
+      <Menu />
 
-
-      <div className={styles.navBar}>
-        {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/menuLogo-Horizontal.png */}
-        <div><Link href="http://google.com"><a>
-          <img className={styles.menuLogo} src="https://i.ibb.co/MPZVFyj/menu-Logo-Horizontal.png" />
-        </a></Link>
-        </div>
-        <div className={styles.groupPage}>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Certificados</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Blog</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Sobre nós</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Contato</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Equipe</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Ex-membros</a></Link></div>
-        </div>
-
-
-
-      </div>
       <div className={styles.mainHeader}>
         <div>
           {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/exmembros/title.png */}
           <div> <img src="https://i.ibb.co/GMSCqJP/title.png" /> </div>
         </div>
-        <div className={styles.legendaSpace}>
+
+        <div className={styles.subtitleSpace}>
           <div className={styles.alignLegend}>
             <div className={styles.infoScrum}></div>
             <div className={styles.scrumMasterStr}><strong>Scrum Master</strong></div>
@@ -44,8 +28,8 @@ export default function ExMembros({ dados }) {
             <div className={styles.intercambioStr}><strong>Intercâmbio</strong></div>
           </div>
         </div>
-
       </div>
+
 
       <div className={styles.bodyGroup}>
         <div className={styles.espHorizontLeft}></div>
@@ -97,35 +81,6 @@ export default function ExMembros({ dados }) {
                 </div>
                 {/* Colocar IF/ELSE (ternário) para exibir apenas nos membros que mandaram relatos  */}
                 <div className={styles.infoRelato}> <strong>VER RELATO</strong></div>
-                <div className={styles.networkGroup}>
-                  {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/social_networks/mail-favicon.png */}
-                  {data.email != "-" ?
-                    <div>
-                      <Link href={'mailto:' + data.email}><a>
-                        <img className={styles.networkFavicon} src="https://i.ibb.co/34FHQD3/mail-favicon.png" />
-                      </a></Link>
-                    </div>
-                    : <div className={styles.networkBlank}></div>}
-
-                  {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/social_networks/lattes-favicon.png */}
-                  {data.lates != "-" ?
-                    <div>
-                      <Link href={data.lates}><a>
-                        <div><img className={styles.networkFavicon} src="https://i.ibb.co/x5FH9NZ/lattes-favicon.png" /></div>
-                      </a></Link>
-                    </div>
-                    : <div className={styles.networkBlank}></div>}
-
-
-                  {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/social_networks/linkedin-favicon.png */}
-                  {data.linkedin != "-" ?
-                    <div>
-                      <Link href={data.linkedin}><a>
-                        <div><img className={styles.networkFavicon} src="https://i.ibb.co/6DThdTt/linkedin-favicon.png" /></div>
-                      </a></Link>
-                    </div>
-                    : <div className={styles.networkBlank}></div>}
-                </div>
               </div>
               : <></>
 
