468dbaade760f6240e625fbbbc532c8ce9dfa93b
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-07-27 14:44:10 -0300

    Adição área de scrum master na pag. da equipe

--- pages/equipe.tsx
+++ pages/equipe.tsx
@@ -5,7 +5,7 @@ import Menu from './menu'
 import Link from 'next/link'
 
 
-export default function Equipe({ dados }) {
+export default function Equipe({ membros, sMaster }) {
   let memberLoad = 8;
   function loadMore() {
     memberLoad = memberLoad + 8;
@@ -35,11 +35,102 @@ export default function Equipe({ dados }) {
         </div>
       </div>
 
+      {/*
+      
+      Espaço do Scrum Master Atual:
+      
+      */}
+      <div className={styles.titleBody}><strong> Scrum Master </strong></div>
+      <div className={styles.bodyGroup}>
+        <div className={styles.espHorizontLeft}></div>
+        <div className={styles.membersArea}>
+          {sMaster.map(data => (firstBlank_space = data.nome.indexOf(' '),
+            lastBlank_space = data.nome.lastIndexOf(' '),
+
+            <div className={styles.membersCard}>
+              <div className={styles.areaPhoto}>
+
+                <div className={styles.infoPhoto}>
+                  <div className={styles.container}>
+                    <div>
+                      {data.scrum_master == false ? <></> :
+                        <div className={styles.alignPhotoArea}>
+                          <div className={styles.infoScrum}></div>
+                          <div className={styles.alignPhotoArea2}></div>
+                        </div>
+                      }
+                      {data.intercambio == false ? <></> :
+                        <div className={styles.infoIntercamb}></div>
+                      }
+                    </div>
+                    {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/exmembros/default-photo.webp */}
+                    <div>
+                      {/* 
+                        {data.photo == "" ? <img className={styles.foto} src="https://i.ibb.co/3swTqhQ/default-photo.webp" />
+                          : <img className={styles.foto} src={data.photo} />}
+                       */}
+                      <img className={styles.foto} src="https://i.ibb.co/3swTqhQ/default-photo.webp" />
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+              <p className={styles.infoName}> <strong>{data.nome.substring(0, firstBlank_space) + ' ' +
+                data.nome.substring(lastBlank_space, data.nome.length)}</strong></p>
+
+
+              <div className={styles.infoCompet}>
+                {data.data_fim.split("-")[0] != data.data_inicio.split("-")[0] ?
+                  <div> COMPET <strong className={styles.infoCompetNUM}>{data.data_inicio.split("-")[0]} </strong> </div> :
+                  <div> COMPET <strong className={styles.infoCompetNUM}>{data.data_inicio.split("-")[0]} </strong> </div>}
+              </div>
+              <div className={styles.networkGroup}>
+                {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/social_networks/mail-favicon.png */}
+                {data.email != "-" ?
+                  <div>
+                    <Link href={'mailto:' + data.email}><a>
+                      <img className={styles.networkFavicon} src="https://i.ibb.co/SfH36np/mail-favicon.png" />
+                    </a></Link>
+                  </div>
+                  : <div className={styles.networkBlank}></div>}
+
+                {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/social_networks/lattes-favicon.png */}
+                {data.lates != "-" ?
+                  <div>
+                    <Link href={data.lates}><a>
+                      <div><img className={styles.networkFavicon} src="https://i.ibb.co/x5FH9NZ/lattes-favicon.png" /></div>
+                    </a></Link>
+                  </div>
+                  : <div className={styles.networkBlank}></div>}
+
+
+                {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/social_networks/linkedin-favicon.png */}
+                {data.linkedin != "-" ?
+                  <div>
+                    <Link href={data.linkedin}><a>
+                      <div><img className={styles.networkFavicon} src="https://i.ibb.co/6DThdTt/linkedin-favicon.png" /></div>
+                    </a></Link>
+                  </div>
+                  : <div className={styles.networkBlank}></div>}
+              </div>
+            </div>
+
+          ))}
+        </div>
+        <div className={styles.espHorizontRight}></div>
+      </div>
+
+      {/*
+      
+      Espaço dos membros atuais:
+      
+      */}
+      <div className={styles.titleBodyMembers}><strong> Membros </strong></div>
       <div className={styles.bodyGroup}>
         <div className={styles.espHorizontLeft}></div>
 
         <div className={styles.membersArea}>
-          {dados.slice(0, memberLoad).map(data => (firstBlank_space = data.nome.indexOf(' '),
+          {membros.slice(0, memberLoad).map(data => (firstBlank_space = data.nome.indexOf(' '),
             lastBlank_space = data.nome.lastIndexOf(' '),
 
             <div className={styles.membersCard}>
@@ -124,11 +215,17 @@ Equipe.getInitialProps = async () => {
     'http://localhost:3000/api/membros'
   );
 
-  let exMembros = [{}];
+  let membrosAtuais = [{}];
+  let scrumMaster = [{}]
+
   let i = 0;
+  let j = 0;
   for (var k in response.data) {
-    if (response.data[k].membro_ativo == true) {
-      exMembros[i] = response.data[k];
+    if (response.data[k].membro_ativo == true && response.data[k].scrum_master == true) {
+      scrumMaster[j] = response.data[k];
+    }
+    if (response.data[k].membro_ativo == true && response.data[k].scrum_master == false) {
+      membrosAtuais[i] = response.data[k];
       i++;
     }
   }
@@ -140,9 +237,9 @@ Equipe.getInitialProps = async () => {
       return 1;
     return 0;
   }
-  exMembros.sort(byName)
-  console.log(exMembros.sort(byName))
-  return { dados: exMembros }
+  membrosAtuais.sort(byName)
+  console.log(membrosAtuais.sort(byName))
+  return { membros: membrosAtuais, sMaster: scrumMaster }
 };
 
 let firstBlank_space;
