c7644d9d60bab43f17ac4a3691bdbbfa5b1c70e7
Francisco <franciscoabreu1408@gmail.com>
2021-08-03 16:27:26 -0300

    Andamento da pagina de certificados

--- pages/certificados.tsx
+++ pages/certificados.tsx
@@ -1,36 +1,58 @@
 import axios from "axios";
 import styles from "../styles/Certificados.module.css";
 import Link from "next/link";
+import Menu from './menu'
 
-export default function Certificados(dados) {
+export default function Certificados({dados}) {
+
   return (
     <div className={styles.groupDiv}>
       <title>COMPET | Certificados</title>
 
-      <div className={styles.navBar}>
-        {/* Adicionar o caminho relativo correto: ---->    ../styles/imgs/menuLogo-Horizontal.png */}
-        <div><Link href="http://google.com"><a>
-          <img className={styles.menuLogo} src="https://i.ibb.co/MPZVFyj/menu-Logo-Horizontal.png" />
-        </a></Link>
+{/* Bloco que contém o Menu da página ---> Ver constução no menu.tsx */}
+      <Menu />
+
+{/* Bloco que contém o Cabeçalho da página */}
+      <div className = {styles.mainHeader}>
+          <div><img src="https://i.ibb.co/GMSCqJP/title.png"/></div>
+      </div>
+
+      <div className={styles.bodyGroup}>
+        <div className={styles.espHorizontLeft}></div>
+
+        <div className={styles.certificadoArea}>
+          {dados.map(certificado => (
+              <div key = {certificado.id} className={styles.certificadoCard}>
+                <div className={styles.areaPhoto}>
+
+                  <div className={styles.infoPhoto}>
+                    <div className={styles.container}>
+                      {/*<img className = {styles.photo} src = {loadPhotos(certificado.compet_talks, certificado.compbio)} /> */} 
+                     <img className = {styles.foto} src = "https://i.ibb.co/3swTqhQ/default-photo.webp" />
+                    </div>
+
+                  </div>    
+                </div>
+                  <div className = {styles.infoName}>
+                <h1>TESTE</h1>
+                </div>
+              </div>
+          ))}
         </div>
-        <div className={styles.groupPage}>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Certificados</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Blog</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Sobre nós</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Contato</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Equipe</a></Link></div>
-          <div className={styles.singlePage}><Link href="http://google.com"><a>Ex-membros</a></Link></div>
-        </div>
-      </div>      
-
-
-
-      {console.log(dados)}
-
-
-
+        <div className = {styles.espHorizontRight}></div>
+      </div>
     </div>
   );
+}
+
+function loadPhotos(CompetTalks:boolean, CompBio:boolean) {
+  if(CompetTalks){
+    return "../styles/imgs/certificados/Compet_Talks.png"
+  }else if(CompBio){
+    return "../styles/imgs/certificados/CompBio.png"
+  }else{
+    return "../styles/imgs/certificados/Certificados_Padrao.png"
+  }
 }
 
 Certificados.getInitialProps = async () => {
@@ -38,3 +60,6 @@ Certificados.getInitialProps = async () => {
 
   return { dados: response.data };
 };
+
+let firstBlank_space;
+let lastBlank_space;
