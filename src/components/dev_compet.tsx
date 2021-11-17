29ecdb1ec255a0a3e7479a991ce27fc2f387bebf
Nataliabgf <luaniasmin@hotmail.com>
2021-11-17 17:34:38 -0300

    primeiro commit

---
+++ src/components/dev_compet.tsx
@@ -0,0 +1,19 @@
+//sem receber parametros:
+export default function natalia(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+//Recebendo parmetros
+export default function natalia({ Parametro_1, Parametro_2 }) {
+    return (
+      <div>
+        
+      </div>
+    )
+    
+  }
+  
