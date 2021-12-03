a7ee86192fbb2856bda87f298572993f897f2ae6
Julio Cesar Rocha <julio.1009@hotmail.com>
2021-12-03 12:34:49 -0300

    Conexão com a coleção 'InterPet' do banco de dados

--- src/pages/InterPet.tsx
+++ src/pages/InterPet.tsx
@@ -1,6 +1,16 @@
+import axios from "axios"
+
 import Menu from '../components/menu'
 import Footer from '../components/footer'
+
 import styles from '../styles/InterPet.module.css'
+
+
+
+InterPet.getInitialProps = async () => {
+  const response = await axios.get("http://localhost:3000/api/interpet");
+  return { dados: response.data };
+};
 
 export default function InterPet() {
   return (
