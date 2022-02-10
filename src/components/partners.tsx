59d293cc37e5f7dae74252e375d20af3f12dc711
Pedro César Mesquita Ferreira <pedrocmferreira@gmail.com>
2022-02-10 00:35:18 -0300

    Create Partners component

---
+++ src/components/partners.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Slider, {SlideInterface} from './slider'
+import SectionTitle from './sectionTitle'
+
+import styles from '../styles/Partners.module.css'
+
+const partners: SlideInterface[] = [
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812597.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812599.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812601.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812606.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812608.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812611.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812614.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812617.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812620.png' },
+    { name: '', url: '', imgUrl: 'https://cdn-icons-png.flaticon.com/128/6812/6812624.png' },
+]
+
+const Partners: React.FC = () => (
+    <div className={styles.partners} >
+        <SectionTitle title="Parcerias" />
+        <div className={styles.partnersSlider} >
+            <Slider slides={partners} slidesToShow={3.5} />
+        </div>
+    </div>
+)
+
+export default Partners
