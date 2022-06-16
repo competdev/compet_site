a7208f21b252294ec7adbd59354f31e51fe48403
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-16 17:00:09 -0300

    Revert "Adicionando semântica HTML a lista de contatos do dirgrad"
    This reverts commit 84cedadc2530230cd8e651cb197a277ef9ea0196.

--- components/ContactInterPet/index.tsx
+++ components/ContactInterPet/index.tsx
@@ -6,28 +6,23 @@ import SectionTitle from '../SectionTitle';
 export default function ContactInterPet() {
     const sectionTitle = "Contato"
     return (
-        <section className={styles.container}>
+        <div className={styles.container}>
             <SectionTitle title={sectionTitle} />
             <div className={styles.card}>
                 <div className={styles.title}>DIRGRAD</div>
                 <div className={styles.cardContent}>
                     <div className={styles.containerContato}>{renderContato()}</div>
                     <div className={styles.addressCEFET}>
-                        <address className={styles.addressText}>
+                        <p className={styles.addressText}>
                             Campus Nova Suíça - Avenida Amazonas, Nº 5253 - Bairro Nova Suíça
-                        </address>
-                        <ul className={styles.contact}>
-                            <li>    
-                                <p> Email :<a href="mailto:dirgrad@cefetmg.br"> dirgrad@cefetmg.br</a></p>
-                            </li>
-                            <li>
-                                <p>Telefone :<a href="tel:+553133197033"> (31) 3319-7033</a></p>
-                            </li>
-                        </ul>
+                        </p>
+                        <p className={styles.addressEmail}>
+                            <a href="mailto:dirgrad@cefetmg.br"> dirgrad@cefetmg.br</a> - (31) 3319-7033
+                        </p>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
 
     )
 }
