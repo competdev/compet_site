152285744f8be019a7c1a63e486637c6527f55f7
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-17 10:58:11 -0300

    Adicionando estilização um link para a página sobre o compet na seção de resumo sobre o compet

--- components/AboutCOMPET/index.tsx
+++ components/AboutCOMPET/index.tsx
@@ -11,7 +11,7 @@ export default function AboutCOMPET() {
 const renderSobre = () => {
   return (
     <section id="about">
-      <SectionTitle title={"Sobre nós"} />
+      <SectionTitle title={"Sobre nós"}/>
       <div className={styles.aboutcard}>
         <div className={styles.img_InterPet}>
           <img src="https://i.ibb.co/HFHt1Kq/0006.png" alt="" />
@@ -21,6 +21,7 @@ const renderSobre = () => {
           disseminar informação técnica, construir soluções envolvendo tecnologias computacionais, organizar eventos de cunho tecnológico, estimular
           a execução de trabalhos científicos na área, engajar os alunos da instituição nos respectivos cursos e realizar atividades sociais.
           </p>
+          <p className={styles.aboutLink}><a href="/sobre">Saiba Mais</a></p>
         </article>
       </div>
     </section>
