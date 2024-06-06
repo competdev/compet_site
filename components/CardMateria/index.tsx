c0f9dff3383bfe1ab72111fed3a6e13a82870648
Heitor <heitorhab123@gmail.com>
2024-06-06 04:50:28 -0300

    funcao fez funcionando

--- components/CardMateria/index.tsx
+++ components/CardMateria/index.tsx
@@ -3,41 +3,18 @@ import styles from "./CardMateria.module.css"
 import { useState } from 'react'
 
 interface ICardMateriaProps {
-    nome: string
-    periodo: string
-    natureza: string
-    tipo:string
-}
-export default function CardMateria({
-    nome,
-    periodo,
-    natureza,
-    tipo
-    }: ICardMateriaProps){
-    const cor = ["grey","green","blue","red"];
-    const [opcao,setOpcao]=useState<number>();
-    function mudaCor(){
-        if(tipo=="fez"){
-            setOpcao(1);
-        }
-        else if(tipo=="pretende"){
-            setOpcao(2);
-        }
-        else if(tipo=="trancou"){
-            setOpcao(3);
-        }
-        else{
-            setOpcao(0);
-        }
+    props:{
+        nome: string
+        periodo: string
+        natureza: string
     }
+}
+export default function CardMateria({props}: ICardMateriaProps){
     return (
-        <div className={styles.cardMateria}
-            onClick={mudaCor}
-            style={{backgroundColor:cor[opcao]}}
-        >
-            <div>{nome}</div>
-            <div>{periodo}</div>
-            <div>{natureza}</div>
+        <div className={styles.cardMateria}>
+            <div>{props.nome}</div>
+            <div>{props.periodo}</div>
+            <div>{props.natureza}</div>
         </div>
     )
 }
--- pages/fluxo_materias/index.tsx
+++ pages/fluxo_materias/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import axios from "axios";
 import { NEXT_URL } from "../../util/config";
@@ -15,51 +15,142 @@ Fluxo_materias.getInitialProps = async () => {
 }
 
 export default function Fluxo_materias({dados}) {
-    console.log(dados)
     const periodos = ["1","2","3","4","5","6","7","8","9","10"]
-    const [tipo,setTipo] = useState<string>()
+    const cor = ["","green","blue","red","grey"];
+    const vetInicialTipos = dados.map(dado=>{
+            if(dado.prerequisitos.length){return(4)}
+            return(0)
+        }
+    )
+    const vetInicialHabilitado = vetInicialTipos.map(dado=>{
+            if(dado==4){return("none")}
+            return("")
+        }
+    )
+    const [tipos, setTipos] = useState<number[]>(vetInicialTipos);
+    const [habilitados, setHabilitados] = useState<string[]>(vetInicialHabilitado);
+    let contador = -1;
+    const [tentativa, setTentativa] = useState(0)
+    const materias = dados.map(
+        dado=>{
+            contador++
+            return(
+                {
+                    pos:contador,
+                    nome:dado.nome,
+                    natureza:dado.natureza,
+                    periodo:dado.periodo,
+                    prerequisitos:dado.prerequisitos,
+                    tipo:tipos[contador],
+                    habilitado:habilitados[contador]
+                }
+            )
+        }
+    )
+    const [tipo,setTipo] = useState<number>(0)
+    React.useEffect(()=>{
+        setTipos(tipos)
+        setHabilitados(habilitados)
+    },[tentativa])
     function handleChange(event) {
         setTipo(event.target.value);
-    };
+    }
+    function mudarTipos(novosTipos:number[]){
+        setTipos(novosTipos)
+        if(tentativa==novosTipos[0]){
+            setTentativa(10)
+        }
+        else{
+            setTentativa(novosTipos[0])
+        }
+    }
+    function mudarHabilitados(novosHabilitados:string[]){
+        setHabilitados(novosHabilitados)
+        if(tentativa==novosHabilitados[0].length){
+            setTentativa(10)
+        }
+        else{
+            setTentativa(novosHabilitados[0].length)
+        }
+    }
+    function fezMateria(nome: string, pos:number){
+        materias.forEach((materia) => {
+            if(materia.prerequisitos.length){
+                let index = materia.prerequisitos.indexOf(nome);
+                if (index > -1) {
+                    materia.prerequisitos.splice(index, 1);
+                    fezMateria(nome,pos)
+                }
+            }
+            else{
+                const novosTipos = tipos
+                novosTipos[materia.pos]=novosTipos[materia.pos]==1?1:0
+                mudarTipos(novosTipos)
+                const novosHabilitados = habilitados
+                novosHabilitados[materia.pos]=""
+                mudarHabilitados(novosHabilitados)
+            }
+        });
+    }
     return (
         <section className={styles.fluxoMaterias}>
             <Header />
             <div className={styles.opcoes}>
                 <label>
                     <input 
-                    type="radio" className={styles.fez} name="etapa" value="fez"
+                    type="radio" className={styles.fez} name="etapa" value={1}
                     onChange={handleChange}
                     />
                     fez
                 </label>
                 <label>
                     <input
-                    type="radio" className={styles.pretende} name="etapa" value="pretende"
+                    type="radio" className={styles.pretende} name="etapa" value={2}
                     onChange={handleChange}
                     />
                     pretende
                 </label>
                 <label>
                     <input
-                    type="radio" className={styles.trancou} name="etapa" value="trancou"
+                    type="radio" className={styles.trancou} name="etapa" value={3}
                     onChange={handleChange}
                     />
                     trancou
                 </label>
             </div>
+            
             <div className={styles.materias}>
                 {periodos.map(periodo=>{
                     return(
                     <div className={styles.materiasColuna}>
-                        {dados.map(data=>{
-                            if(data.natureza=="OB" && data.periodo==periodo){
+                        {materias.map(materia=>{
+                            if(materia.natureza=="OB" && materia.periodo==periodo){
                                 return(
-                                    <CardMateria 
-                                        nome={data.nome}
-                                        periodo={data.periodo}
-                                        natureza={data.natureza}
-                                        tipo={tipo}
-                                    />
+                                    <div key={materia.nome}
+                                        onClick={()=>{
+                                            const novosTipos = tipos
+                                            novosTipos[materia.pos]=tipo
+                                            mudarTipos(novosTipos)
+                                            if(tipo==1){
+                                                fezMateria(materia.nome,materia.pos)
+                                            }
+                                        }}
+                                    
+                                        style={
+                                            {
+                                                backgroundColor:cor[materia.tipo],
+                                                pointerEvents:materia.habilitado
+                                            }
+                                        }    
+                                    >
+                                        <CardMateria
+                                            props={{
+                                                nome:materia.nome,
+                                                periodo:materia.periodo,
+                                                natureza:materia.natureza,
+                                            }}
+                                        />
+                                    </div>
                                 )
                             }
                         })}
@@ -74,15 +165,23 @@ export default function Fluxo_materias({dados}) {
                 {periodos.map(periodo=>{
                     return(
                     <div className={styles.materiasColuna}>
-                        {dados.map(data=>{
-                            if(data.natureza!="OB" && data.periodo==periodo){
+                        {materias.map(materia=>{
+                            if(materia.natureza!="OB" && materia.periodo==periodo){
                                 return(
-                                    <CardMateria 
-                                        nome={data.nome}
-                                        periodo={data.periodo}
-                                        natureza={data.natureza}
-                                        tipo={tipo}
-                                    />
+                                    <div key={materia.nome}
+                                    onClick={()=>{
+                                        let novosTipos = tipos
+                                        novosTipos[materia.pos]=tipo
+                                        setTipos(novosTipos)
+                                    }}>
+                                        <CardMateria key={materia.nome}
+                                            props={{
+                                                nome:materia.nome,
+                                                periodo:materia.periodo,
+                                                natureza:materia.natureza,
+                                            }}
+                                        />
+                                    </div>
                                 )
                             }
                         })}
