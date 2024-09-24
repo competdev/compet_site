4a9ae297bb8c2f956917ccf1d0cbe60d10957b1e
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2024-09-24 16:14:14 -0300

    Arrumando

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import "../styles/Globals.css"
 import type { AppProps } from "next/app"
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    <>
-        return <Component {...pageProps} />
-        <Analytics />
-    </>
-    
+    return <Component {...pageProps} />
 }
 export default MyApp
